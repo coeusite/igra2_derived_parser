8faefa7f30c32ac9217b9ec534e9a9e726a66c21
CoeusITE <coeusite@gmail.com>
2016-09-08 10:56:12 +0800

    create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='igra2_derived_parser',
+      version='0.0.1',
+      author_email='coeusite@gmail.com',
+      description='Parser for NOAA IGRA2 Derived Files (for Python 3.5) ',
+      author='coeusite',
+      url='https://github.com/coeusite/igra2_derived_parser',
+      packages=['igra2_derived_parser',],
+      package_dir={'igra2_derived_parser': 'src'},
+      )
